32470f3b2a7f100cc05d7c5e8094466467581975
subhaz18 <psubhash2004@gmail.com>
2024-08-19 17:17:40 +0530

    changes

--- js/contact.js
+++ js/contact.js
@@ -1,13 +1,18 @@
 function sendmail(){
-  let parms={
-    name : document.getElementById("firstname").value,
-    lname : document.getElementById("lastname").value,
-    email : document.getElementById("email").value,
-    subject : document.getElementById("subject").value,
-    message : document.getElementById("message").value
 
-  }
-  console.log(parms);
-  emailjs.send("service_yfx1cth","template_wab7625",parms).then(alert("Email sent !!"))
+   const name = document.getElementById("firstname").value
+   const lname = document.getElementById("lastname").value
+   const email = document.getElementById("email").value
+   const subject = document.getElementById("subject").value
+  const message = document.getElementById("message").value
+
+  emailjs.send("service_yfx1cth","template_wab7625",{
+    name: "subhash",
+    lname: "p",
+    email: "abc@hjghj",
+    subject: "fe",
+    message: "hello",
+    reply_to: "psubhash@gmail.com"
+    }).then(alert("Email sent !!"))
 
 }
